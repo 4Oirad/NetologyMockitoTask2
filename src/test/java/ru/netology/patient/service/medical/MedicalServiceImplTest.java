177a3f9dfd4df8cce400d06ec8e132aa74d12336
Bair Arshaev <ars-bair@yandex.ru>
2022-06-14 15:46:27 +0300

    Edit code 2

--- src/test/java/ru/netology/patient/service/medical/MedicalServiceImplTest.java
+++ src/test/java/ru/netology/patient/service/medical/MedicalServiceImplTest.java
@@ -2,21 +2,65 @@ package ru.netology.patient.service.medical;
 
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import ru.netology.patient.entity.BloodPressure;
+import ru.netology.patient.entity.HealthInfo;
 import ru.netology.patient.entity.PatientInfo;
 import ru.netology.patient.repository.PatientInfoFileRepository;
 import ru.netology.patient.repository.PatientInfoRepository;
+import ru.netology.patient.service.alert.SendAlertService;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class MedicalServiceImplTest {
 
+    PatientInfo patientInfo = new PatientInfo(
+            "123",
+            "Ivan",
+            "Ivanov",
+            LocalDate.of(1975, 03, 28),
+            new HealthInfo(new BigDecimal("36.65"), new BloodPressure(120, 80))
+    );
+
     @Test
-    void checkBloodPressureTest() {
-        PatientInfoRepository patientInfoRepository = Mockito.mock(PatientInfoFileRepository.class);
-        patientInfoRepository.
+    void checkBloodPressureShouldSendMessage() {
+        PatientInfoRepository patientInfoFileRepository = Mockito.mock(PatientInfoFileRepository.class);
+        SendAlertService sendAlertService = Mockito.mock(SendAlertService.class);
+        Mockito.when(patientInfoFileRepository.getById("123")).thenReturn(patientInfo);
+        MedicalService medicalService = new MedicalServiceImpl(patientInfoFileRepository, sendAlertService);
+        medicalService.checkBloodPressure(patientInfo.getId(), new BloodPressure(135, 80));
+        Mockito.verify(sendAlertService, Mockito.atLeastOnce()).send(Mockito.anyString());
     }
 
     @Test
-    void checkTemperature() {
+    void checkBloodPressureShouldNotSendMessage() {
+        PatientInfoRepository patientInfoFileRepository = Mockito.mock(PatientInfoFileRepository.class);
+        SendAlertService sendAlertService = Mockito.mock(SendAlertService.class);
+        Mockito.when(patientInfoFileRepository.getById("123")).thenReturn(patientInfo);
+        MedicalService medicalService = new MedicalServiceImpl(patientInfoFileRepository, sendAlertService);
+        medicalService.checkBloodPressure(patientInfo.getId(), new BloodPressure(120, 80));
+        Mockito.verify(sendAlertService, Mockito.never()).send(Mockito.anyString());
+    }
+
+    @Test
+    void checkTemperatureShouldSendMessage() {
+        PatientInfoRepository patientInfoFileRepository = Mockito.mock(PatientInfoFileRepository.class);
+        SendAlertService sendAlertService = Mockito.mock(SendAlertService.class);
+        Mockito.when(patientInfoFileRepository.getById("123")).thenReturn(patientInfo);
+        MedicalService medicalService = new MedicalServiceImpl(patientInfoFileRepository, sendAlertService);
+        medicalService.checkTemperature(patientInfo.getId(), new BigDecimal("35"));
+        Mockito.verify(sendAlertService, Mockito.atLeastOnce()).send(Mockito.anyString());
+    }
+
+    @Test
+    void checkTemperatureShouldNotSendMessage() {
+        PatientInfoRepository patientInfoFileRepository = Mockito.mock(PatientInfoFileRepository.class);
+        SendAlertService sendAlertService = Mockito.mock(SendAlertService.class);
+        Mockito.when(patientInfoFileRepository.getById("123")).thenReturn(patientInfo);
+        MedicalService medicalService = new MedicalServiceImpl(patientInfoFileRepository, sendAlertService);
+        medicalService.checkTemperature(patientInfo.getId(), new BigDecimal("36.6"));
+        Mockito.verify(sendAlertService, Mockito.never()).send(Mockito.anyString());
     }
 }
